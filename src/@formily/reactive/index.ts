83ea7bec3668970a695c0b400f85bb803ff590dd
zjoney <zdjoey@126.com>
2023-03-01 15:51:05 +0800

    feat: 🎸 实现formily/reactive中的tracker、autorun

--- src/@formily/reactive/index.ts
+++ src/@formily/reactive/index.ts
@@ -1,14 +1,53 @@
-export function observable(value){
-return new Proxy(value, baseHandler);
+let currentReaction;
+const RawReactionsMap = new WeakMap()
+export function observable(value) {
+  return new Proxy(value, baseHandler);
 }
-const baseHandler={
-  get(target, key){
-
+const baseHandler: any = {
+  get(target, key) {
+    const result = target[key];
+    if (currentReaction) { // 当前存在一个响应器
+      addRawReactionMap(target, key, currentReaction)
+    }
+    return result;
   },
-  set(target, key, value){
-
+  set(target, key, value) {
+    target[key] = value;
+    RawReactionsMap.get(target)?.get(key)?.forEach(raction => {
+      raction()
+    });
+    return true
   }
 }
-export function autorun(){
-
+/**
+ * 把此响应器和属性绑定
+ * @param target 对象
+ * @param key 属性
+ * @param reaction 响应器
+ */
+function addRawReactionMap(target, key, reaction) {
+  const reactionsMap = RawReactionsMap.get(target);
+  if (reactionsMap) {
+    const reactions = reactionsMap.get(key);
+    if (reactions) {
+      reactions.push(reaction)
+    } else {
+      reactionsMap.set(key, [reaction])
+    }
+    return reactionsMap;
+  } else {
+    const reactionsMap = new Map();
+    reactionsMap.set(key, [reaction])
+    RawReactionsMap.set(target, reactionsMap)
+    return reactionsMap;
+  }
+}
+export function autorun(tracker) {
+  // reaction作为响应器，它也是一个函数
+  const reaction = () => {
+    currentReaction = reaction
+    tracker()
+    currentReaction = null
+  }
+  reaction()
 }
--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,8 @@ const obs = observable({name: '1'})
  * autorun可以创建自动执行的响应器,
  * 可以接受一个tracker函数，如果函数内有消费者数据，数据发生变化tracker会重新执行
  */
-// const tracker = ()=>{
-//   console.log(obs.name)
-// }
-// autorun(tracker)
+const tracker = ()=>{
+  console.log(obs.name)
+}
+autorun(tracker)
 obs.name = '2'
