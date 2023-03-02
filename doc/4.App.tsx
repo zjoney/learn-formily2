50b37ebe12544aad120bba31185c8703fff95f29
zjoney <zdjoey@126.com>
2023-03-02 14:59:40 +0800

    feat: 🎸 字段协议联动
    3种分别是：主动联动、被动联动、effects联动(推荐写法)

---
+++ doc/4.App.tsx
@@ -0,0 +1,65 @@
+import { createForm, onFormInit, onFormReact } from "@formily/core";
+import { Field, createSchemaField } from "@formily/react";
+import 'antd/dist/antd.css'
+import { Form, FormItem, Input, NumberPicker } from '@formily/antd'
+
+// 联动协议：主动联动、被动联动、effects联动
+// 主动联动
+const form = createForm()
+const SchemaField = createSchemaField({
+  components: {
+    FormItem, Input, NumberPicker
+  }
+});
+
+const schema = {
+  type: 'object',
+  properties: {
+    source: {
+      type: 'string',
+      title: '来源',
+      required: true,
+      'x-decorator': 'FormItem',
+      'x-component': 'Input',
+      'x-component-props': {
+        'placeholder': '请输入source'
+      },
+      'x-reactions': [
+        {
+          'target': 'target', // 要操作的字段路径
+          //代表当前字段实例，可以在普通属性表达式中使用，也能在 x-reactions 中使用
+          'when': "{{$self.value == '123'}}", // 联动条件
+          'fulfill': {//满足条件
+            'state': {//更新状态
+              visible: true
+            }
+          },
+          'otherwise': {//不满足条件
+            'state': {//更新状态
+              visible: false
+            }
+          }
+        }
+      ]
+    },
+    target: {
+      type: 'string',
+      title: '目标',
+      'x-decorator': 'FormItem',
+      'x-component': 'Input',
+      'x-component-props': {
+        'placeholder': '请输入'
+      },
+    }
+  }
+}
+function App() {
+  return (
+    <Form form={form} labelCol={6} wrapperCol={5}>
+      <SchemaField
+        schema={schema}
+      />
+    </Form>
+  )
+}
+export default App;
--- doc/5.App.tsx
+++ doc/5.App.tsx
@@ -1,34 +1,11 @@
-import { createForm, onFieldMount, onFieldValueChange, onFormInit, onFormReact } from "@formily/core";
+import { createForm, onFormInit, onFormReact } from "@formily/core";
 import { Field, createSchemaField } from "@formily/react";
 import 'antd/dist/antd.css'
 import { Form, FormItem, Input, NumberPicker } from '@formily/antd'
 
 // 联动协议：主动联动、被动联动、effects联动
-//  effects联动
-const form = createForm({
-  effects() {//effects 副作用逻辑，用于实现各种联动逻辑
-    //用于监听某个字段已挂载的副作用钩子
-    onFieldMount('target', (field: any) => {
-      form.setFieldState(field.query('target'), (targetState) => {
-        if (field.value == 123) {
-          targetState.visible = true
-        } else {
-          targetState.visible = false
-        }
-      })
-    })
-    //用于监听某个字段值变化的副作用钩子
-    onFieldValueChange('source', (field: any) => {
-      form.setFieldState(field.query('target'), (targetState) => {
-        if (field.value == 123) {
-          targetState.visible = true
-        } else {
-          targetState.visible = false
-        }
-      })
-    })
-  }
-})
+//  被动联动
+const form = createForm()
 const SchemaField = createSchemaField({
   components: {
     FormItem, Input, NumberPicker
@@ -73,24 +50,24 @@ const schema = {
       'x-component-props': {
         'placeholder': '请输入'
       },
-      // 'x-reactions': [
-      //   {
-      //     'dependencies': ['source'],
-      //     // 'target': 'target', // 要操作的字段路径
-      //     //代表当前字段实例，可以在普通属性表达式中使用，也能在 x-reactions 中使用
-      //     'when': "{{$deps[0] == '123'}}", // 联动条件
-      //     'fulfill': {//满足条件
-      //       'state': {//更新状态
-      //         visible: true
-      //       }
-      //     },
-      //     'otherwise': {//不满足条件
-      //       'state': {//更新状态
-      //         visible: false
-      //       }
-      //     }
-      //   }
-      // ]
+      'x-reactions': [
+        {
+          'dependencies': ['source'],
+          // 'target': 'target', // 要操作的字段路径
+          //代表当前字段实例，可以在普通属性表达式中使用，也能在 x-reactions 中使用
+          'when': "{{$deps[0] == '123'}}", // 联动条件
+          'fulfill': {//满足条件
+            'state': {//更新状态
+              visible: true
+            }
+          },
+          'otherwise': {//不满足条件
+            'state': {//更新状态
+              visible: false
+            }
+          }
+        }
+      ]
     }
   }
 }
