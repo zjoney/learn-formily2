cf6494952ca01b4b3fe26e7584637fbd0eb2a977
zjoney <zdjoey@126.com>
2023-03-03 09:55:21 +0800

    feat: 🎸 补充formily的第三写法

--- doc/4.App.tsx
+++ doc/4.App.tsx
@@ -1,64 +1,36 @@
-import { createForm, onFormInit, onFormReact } from "@formily/core";
-import { Field, createSchemaField } from "@formily/react";
+import { createForm } from '@formily/core'
+import { createSchemaField } from '@formily/react'
 import 'antd/dist/antd.css'
 import { Form, FormItem, Input, NumberPicker } from '@formily/antd'
-
-// 联动协议：主动联动、被动联动、effects联动
-// 主动联动
 const form = createForm()
+// 第三种 Markup写法
 const SchemaField = createSchemaField({
   components: {
-    FormItem, Input, NumberPicker
-  }
-});
+    Input,
+    FormItem,
+    NumberPicker
+  },
+})
 
-const schema = {
-  type: 'object',
-  properties: {
-    source: {
-      type: 'string',
-      title: '来源',
-      required: true,
-      'x-decorator': 'FormItem',
-      'x-component': 'Input',
-      'x-component-props': {
-        'placeholder': '请输入source'
-      },
-      'x-reactions': [
-        {
-          'target': 'target', // 要操作的字段路径
-          //代表当前字段实例，可以在普通属性表达式中使用，也能在 x-reactions 中使用
-          'when': "{{$self.value == '123'}}", // 联动条件
-          'fulfill': {//满足条件
-            'state': {//更新状态
-              visible: true
-            }
-          },
-          'otherwise': {//不满足条件
-            'state': {//更新状态
-              visible: false
-            }
-          }
-        }
-      ]
-    },
-    target: {
-      type: 'string',
-      title: '目标',
-      'x-decorator': 'FormItem',
-      'x-component': 'Input',
-      'x-component-props': {
-        'placeholder': '请输入'
-      },
-    }
-  }
-}
 function App() {
   return (
-    <Form form={form} labelCol={6} wrapperCol={5}>
-      <SchemaField
-        schema={schema}
-      />
+    <Form form={form} labelCol={6} wrapperCol={10}>
+      <SchemaField>
+        <SchemaField.String
+          name="name"
+          title="姓名"
+          required
+          x-component="Input"//字段 UI 组件属性
+          x-decorator="FormItem"//字段 UI 包装器组件
+        />
+        <SchemaField.Number
+          name="age"
+          title="年龄"
+          maximum={120}
+          x-component="NumberPicker"//字段 UI 组件属性
+          x-decorator="FormItem"//字段 UI 包装器组件
+        />
+      </SchemaField>
     </Form>
   )
 }
--- doc/6.App.tsx
+++ doc/6.App.tsx
@@ -4,7 +4,7 @@ import 'antd/dist/antd.css'
 import { Form, FormItem, Input, NumberPicker } from '@formily/antd'
 
 // 联动协议：主动联动、被动联动、effects联动
-// 主动联动
+//  被动联动
 const form = createForm()
 const SchemaField = createSchemaField({
   components: {
@@ -24,11 +24,38 @@ const schema = {
       'x-component-props': {
         'placeholder': '请输入source'
       },
+      // 'x-reactions': [
+      //   {
+      //     'target': 'target', // 要操作的字段路径
+      //     //代表当前字段实例，可以在普通属性表达式中使用，也能在 x-reactions 中使用
+      //     'when': "{{$self.value == '123'}}", // 联动条件
+      //     'fulfill': {//满足条件
+      //       'state': {//更新状态
+      //         visible: true
+      //       }
+      //     },
+      //     'otherwise': {//不满足条件
+      //       'state': {//更新状态
+      //         visible: false
+      //       }
+      //     }
+      //   }
+      // ]
+    },
+    target: {
+      type: 'string',
+      title: '目标',
+      'x-decorator': 'FormItem',
+      'x-component': 'Input',
+      'x-component-props': {
+        'placeholder': '请输入'
+      },
       'x-reactions': [
         {
-          'target': 'target', // 要操作的字段路径
+          'dependencies': ['source'],
+          // 'target': 'target', // 要操作的字段路径
           //代表当前字段实例，可以在普通属性表达式中使用，也能在 x-reactions 中使用
-          'when': "{{$self.value == '123'}}", // 联动条件
+          'when': "{{$deps[0] == '123'}}", // 联动条件
           'fulfill': {//满足条件
             'state': {//更新状态
               visible: true
@@ -41,15 +68,6 @@ const schema = {
           }
         }
       ]
-    },
-    target: {
-      type: 'string',
-      title: '目标',
-      'x-decorator': 'FormItem',
-      'x-component': 'Input',
-      'x-component-props': {
-        'placeholder': '请输入'
-      },
     }
   }
 }
